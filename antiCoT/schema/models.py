0062b7364e8e50e6f3abfe33114ada0b86a19c06
John Donovan <mersey.viking@gmail.com>
2013-07-22 20:33:14 +0100

    Added metering point API, brought entity API in line with the AMON API documentation.

--- antiCoT/schema/models.py
+++ antiCoT/schema/models.py
@@ -11,26 +11,48 @@ uuid_pattern = re.compile('[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0
 class Entity(db.Model):
     __tablename__ = 'entities'
 
-    entity_summary_fields = {'entityId': fields.String(attribute='uuid'),
-                             'description': fields.String}
-
-    entity_fields = {'entityId': fields.String(attribute='uuid'),
-                     'description': fields.String}
-
-    _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    uuid = db.Column(db.String(36), unique=True)
+    uuid = db.Column(db.String(36), unique=True, primary_key=True)
     description = db.Column(db.Text(), nullable=True)
+    meteringPointIds = db.relationship('MeteringPoint', cascade='all,delete', backref=db.backref('entity'))
+
+    summary_fields = {'entityId': fields.String(attribute='uuid'),
+                      'description': fields.String}
+
+    fields = {'entityId': fields.String(attribute='uuid'),
+              'description': fields.String,
+              'meteringPointIds': fields.List(fields.String),
+    }
 
     def __init__(self, UUID=None, description=None):
         if UUID is None:
-            self.uuid = str(uuid.uuid4())
+            self.uuid = str(uuid.uuid4()).lower()
         else:
             if uuid_pattern.match(UUID) is None:
                 raise ArgumentError
             else:
-                self.uuid = UUID
+                self.uuid = UUID.lower()
 
         self.description = description
 
     def __repr__(self):
-        return '<Entity {0}>'.format(self.uuid)
+        return self.uuid
+
+
+class MeteringPoint(db.Model):
+    __tablename__ = 'metering_points'
+
+    uuid = db.Column(db.String(36), unique=True, primary_key=True)
+    description = db.Column(db.Text(), nullable=True)
+    entity_id = db.Column(db.String, db.ForeignKey('entities.uuid'))
+
+    fields = {'meteringPointId': fields.String(attribute='uuid'),
+              'entityId': fields.String(attribute='entity'),
+              'description': fields.String}
+
+    def __init__(self, UUID, entity_id, description=None):
+        self.uuid = UUID.lower()
+        self.entity_id = entity_id.lower()
+        self.description = description
+
+    def __repr__(self):
+        return self.uuid
--- antiCoT/views.py
+++ antiCoT/views.py
@@ -1,14 +1,8 @@
 from flask import request, url_for, make_response, jsonify
-from flask.ext.restful import Resource, reqparse, marshal, abort
+from flask.ext.restful import Resource, marshal, abort
 from sqlalchemy.exc import ArgumentError, IntegrityError
 from antiCoT import db, api
-from antiCoT.schema.models import Entity
-
-
-# Initialise the Flask-RESTful argument parser.
-parser = reqparse.RequestParser()
-parser.add_argument('entityId', type=str)
-parser.add_argument('description', type=str)
+from antiCoT.schema.models import Entity, MeteringPoint
 
 
 class Entities(Resource):
@@ -22,21 +16,20 @@ class Entities(Resource):
                  404 - The entity cannot be found
         """
         if entity_id is None:
-            # entities = db.session.query(Entity.uuid).all()
             entities = Entity.query.all()
-            response = jsonify({'entities': marshal(entities, Entity.entity_summary_fields)})
+            response = jsonify({'entities': marshal(entities, Entity.summary_fields)})
         else:
             entities = Entity.query.filter(Entity.uuid == entity_id).all()
-            response = jsonify({'entities': marshal(entities, Entity.entity_fields)})
+            response = jsonify({'entities': marshal(entities, Entity.fields)})
 
-        if len(entities) == 0:
-            abort(404, message='Entity not found')
+            if len(entities) == 0:
+                abort(404, message='Entity not found')
 
         return response
 
     def post(self):
         """
-        Adds a new entity to the database.
+        Adds a new entity to the database. Doesn't support the "?append=true" parameter.
 
         :return: 201 - The entity was successfully created
                  400 - The parameters were malformed
@@ -45,24 +38,22 @@ class Entities(Resource):
         entity_id = None
         description = None
 
-        if request.headers['Content-Type'] == 'application/json':
-            content = request.get_json()
-            if 'entityId' in content:
-                entity_id = content['entityId']
-
-            if 'description' in content:
-                description = content['description']
-        else:
-            args = parser.parse_args()
-            entity_id = args['entityId']
-            description = args['description']
-
         try:
+            if request.headers['Content-Type'] == 'application/json':
+                content = request.get_json()
+                if 'entityId' in content:
+                    entity_id = content['entityId'].lower()
+
+                if 'description' in content:
+                    description = content['description']
+            else:
+                raise ArgumentError
+
             entity = Entity(entity_id, description)
             db.session.add(entity)
             db.session.commit()
 
-            return make_response(('', 201, {'Location': url_for('entity', entity_id=entity.uuid, _external=True)}))
+            return make_response(('', 201, {'Location': url_for('entities', entity_id=entity.uuid, _external=True)}))
         except ArgumentError:
             abort(400, message='Invalid entity parameters')
         except IntegrityError:
@@ -84,9 +75,8 @@ class Entities(Resource):
 
             if 'description' in content:
                 description = content['description']
-        elif request.headers['Content-Type'] == 'application/x-www-form-urlencoded':
-            if 'description' in request.form:
-                description = request.form['description']
+        else:
+            abort(400, message='Invalid entity parameters')
 
         entity = Entity.query.filter(Entity.uuid == entity_id).first()
 
@@ -108,7 +98,8 @@ class Entities(Resource):
         Removes an entity from the database.
 
         :param entity_id: The UUID of the entity being deleted
-        :return: 200 - The
+        :return: 200 - The entity was deleted successfully
+                 404 - The entity was not found
         """
         the_entity = Entity.query.filter(Entity.uuid == entity_id).first()
 
@@ -117,6 +108,118 @@ class Entities(Resource):
 
         db.session.delete(the_entity)
         db.session.commit()
-        return make_response('', 204)
+        return make_response('', 200)
 
-api.add_resource(Entities, '/entity/<entity_id>', '/entity', endpoint='entity')
+
+class MeteringPoints(Resource):
+    def get(self, metering_point_id=None):
+        if metering_point_id is None:
+            abort(400, message='Metering point id missing')
+
+        metering_points = MeteringPoint.query.filter(MeteringPoint.uuid == metering_point_id).all()
+        response = jsonify({'meteringPoints': marshal(metering_points, MeteringPoint.fields)})
+
+        if len(metering_points) == 0:
+            abort(404, message='Metering Point not found')
+
+        return response
+
+    def post(self):
+        """
+        Adds a new metering point to the database.
+
+        :return: 201 - The metering point was successfully created
+                 400 - The parameters were malformed
+                 403 - The metering point already exists
+        """
+        metering_point_id = None
+        description = None
+        entity_id = None
+
+        try:
+            if request.headers['Content-Type'] == 'application/json':
+                content = request.get_json()
+                if 'meteringPointId' in content:
+                    metering_point_id = content['meteringPointId'].lower()
+
+                if 'description' in content:
+                    description = content['description']
+
+                if 'entityId' in content:
+                    entity_id = content['entityId'].lower()
+            else:
+                raise ArgumentError
+
+            metering_point = MeteringPoint(metering_point_id, entity_id, description)
+            db.session.add(metering_point)
+            db.session.commit()
+
+            return make_response(('', 201, {'Location': url_for('meteringpoints', metering_point_id=metering_point.uuid, _external=True)}))
+        except ArgumentError:
+            abort(400, message='Invalid metering point parameters')
+        except IntegrityError:
+            abort(403, message='The metering point already exists')
+
+    def put(self, metering_point_id):
+        """
+        Updates a metering point in the database.
+
+        :param metering_point_id: The UUID of the metering point being updated
+        :return: 200 - The metering point was updated
+                 201 - A new metering point was created
+                 400 - The input was malformed
+        """
+        description = None
+        entity_id = None
+
+        if request.headers['Content-Type'] == 'application/json':
+            content = request.get_json()
+
+            if 'description' in content:
+                description = content['description']
+
+            if 'entityId' in content:
+                entity_id = content['entityId'].lower()
+        else:
+            abort(400, message='Invalid entity parameters')
+
+        metering_point = MeteringPoint.query.filter(MeteringPoint.uuid == metering_point_id).first()
+
+        if metering_point is None:
+            try:
+                metering_point = MeteringPoint(metering_point_id, entity_id, description)
+                db.session.add(metering_point)
+                return make_response(('', 201, {'Location': url_for('meteringpoints', metering_point_id=metering_point.uuid, _external=True)}))
+            except ArgumentError:
+                abort(400, message='Invalid entity parameters')
+
+        else:
+            metering_point.description = description
+
+            if entity_id is not None and entity_id != metering_point.entity_id:
+                # We may need to do something clever here with re-parenting this record.
+                metering_point.entity_id = entity_id
+
+            db.session.commit()
+            return make_response('', 200)
+
+    def delete(self, metering_point_id):
+        """
+        Removes an entity from the database.
+
+        :param metering_point_id: The UUID of the metering point being deleted
+        :return: 200 - The metering point was deleted successfully
+                 404 - The metering point was not found
+        """
+        the_metering_point = MeteringPoint.query.filter(MeteringPoint.uuid == metering_point_id).first()
+
+        if the_metering_point is None:
+            abort(404, message='Unknown meteringPointId')
+
+        db.session.delete(the_metering_point)
+        db.session.commit()
+        return make_response('', 200)
+
+
+api.add_resource(Entities, '/entities/<entity_id>', '/entities', endpoint='entities')
+api.add_resource(MeteringPoints, '/metering-points/<metering_point_id>', '/metering-points', endpoint='meteringpoints')
